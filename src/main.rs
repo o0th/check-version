a38775dfcc99c159c58f9f1c721b5316300afb9d
o0th <o0th@pm.me>
2023-12-08 11:40:22 +0100

    feat: testing action

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-use std::env;
+use std::{env, process::ExitCode};
 
-fn main() {
+fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    if args.len() < 5 {
+        eprintln!("Usage: {} <repository> <from> <to> <file>", args[0]);
+        return ExitCode::FAILURE;
+    }
+
+    let repository = &args[1];
+    let from = &args[2];
+    let to = &args[3];
+    let file = &args[4];
+
+    println!("Repository: {}", repository);
+    println!("From: {}", from);
+    println!("To: {}", to);
+    println!("File: {}", file);
+
+    return ExitCode::SUCCESS;
 }
